fbe73030e2b709efb3d35cc26ff2c9e396d029e7
fi-le <42694673+file-acomplaint@users.noreply.github.com>
2021-10-24 21:30:00 +0200

    Improved MacOS support

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,16 @@
-// author   : fi-le
+// author   : fi-le (she/her)
 // website  : fi-le.net
 // e-mail   : info @t fi-le.net
 // license  : MIT
 
 use std::io::{stdout, Write};
 use std::time::Duration;
-
+use std::env;
 use crossterm::{
     style::{Color, Print, SetForegroundColor},
     ExecutableCommand,
     QueueableCommand,
     event::Event,
-    event::KeyCode,
     cursor,
     terminal,
     event::{read, poll}
@@ -23,7 +22,7 @@ fn main() {
     let mut stdout = stdout();
 
     stdout.execute(terminal::Clear(terminal::ClearType::All)).ok();
-    stdout.execute(terminal::SetTitle("SIMUWAERM v0.01")).ok();     // Inexplicably doesn't work on UNIX
+    stdout.execute(terminal::SetTitle("SIMUWAERM v0.02")).ok();     // Inexplicably doesn't work on UNIX
     stdout.execute(terminal::SetSize(65, 40)).ok();                 // Inexplicably doesn't work on UNIX
 
     const WIDTH : usize = 25;
@@ -44,7 +43,7 @@ fn main() {
     let mut counter : u8 = 0; // We refresh once in a while as UNIX terminals like to leave fragments. Windows terminals don't handle clearing the screen well, so we only do it when needed. Conditional compilation would be the better option.
 
     loop {
-        if (screen_dimension != terminal::size().unwrap() || counter == 150) {
+        if (screen_dimension != terminal::size().unwrap() || counter == 150 || env::consts::OS == "linux") {
             stdout.queue(terminal::Clear(terminal::ClearType::All)).ok();
             screen_dimension = terminal::size().unwrap();
             counter = 0; // Deliberate integer overflow would have been fun, but it's of course not very Rust-y.
@@ -57,7 +56,7 @@ fn main() {
         stdout.queue(cursor::MoveTo(0,3)).ok();
 
         if (language == "de") {
-            stdout.queue(Print("     SIMUWAERM v0.01\n")).ok();
+            stdout.queue(Print("     SIMUWAERM v0.02\n")).ok();
             stdout.queue(Print("     --------- -----\n")).ok();
             stdout.queue(Print("     Cursor kann mit Pfeiltasten bewegt werden.             \n")).ok();
             stdout.queue(Print("     h - erwärmen | + - Lambda erhöhen    | Esc - Beenden   \n")).ok();
@@ -66,11 +65,11 @@ fn main() {
         }
 
         else if (language == "en") {
-            stdout.queue(Print("     SIMUWAERM v0.01\n")).ok();
+            stdout.queue(Print("     SIMUWAERM v0.02\n")).ok();
             stdout.queue(Print("     --------- -----\n")).ok();
             stdout.queue(Print("     Cursor is moved with arrow keys.                       \n")).ok();
             stdout.queue(Print("     h - heat | + - increase Lambda    | Esc - Exit         \n")).ok();
-            stdout.queue(Print("     k - cool | - - decrease Lambda    | p   - Pause        \n\n")).ok();
+            stdout.queue(Print("     c - cool | - - decrease Lambda    | p   - Pause        \n\n")).ok();
             stdout.queue(Print(format!("     \tLambda = {:.2}                               \n", lambda))).ok();
         }
 
@@ -79,10 +78,20 @@ fn main() {
 
             for j in 0..HEIGHT {
                 if [j, i] == cursor {
-                    stdout.queue(SetForegroundColor(Color::Rgb{r:100, g: 250, b: 100})).ok();
+                    if (env::consts::OS == "macos") {
+                        stdout.queue(SetForegroundColor(Color::AnsiValue(83))).ok();
+                    }
+                    else {
+                        stdout.queue(SetForegroundColor(Color::Rgb{r:100, g: 250, b: 100})).ok();
+                    }
                 }
                 else {
-                    stdout.queue(SetForegroundColor(Color::Rgb{r:(plate[i + WIDTH * j] as u8), g: 100, b: (255. - plate[i + WIDTH * j]) as u8})).ok();
+                    if (env::consts::OS == "macos") {
+                        stdout.queue(SetForegroundColor(Color::AnsiValue(22 + (36 * ((plate[i + WIDTH * j] as u8) / 51)) + ((255. - plate[i + WIDTH * j]) as u8) / 51))).ok();
+                    }
+                    else {
+                        stdout.queue(SetForegroundColor(Color::Rgb{r:(plate[i + WIDTH * j] as u8), g: 100, b: (255. - plate[i + WIDTH * j]) as u8})).ok();
+                    }
                 }
 
                 stdout.queue(Print('█')).ok();
@@ -117,81 +126,72 @@ fn main() {
                 plate_p[j + WIDTH * i] -= (lambda * ((plate[j + WIDTH * i] - neighbor)));
             }
         }
-        stdout.queue(cursor::MoveTo(15,3)).ok(); // The cursor is still displayed, so we move it onto somewhere pretty
-        stdout.flush().ok();
-
         plate = plate_p.to_vec();
 
         crossterm::terminal::enable_raw_mode().ok(); // For UNIX terminals
 
         if poll(Duration::from_millis(100)).unwrap() {
             let event = read().unwrap();
-
-            if (event == Event::Key(KeyCode::Left.into())) {
-                if cursor[0] > 0    {
-                    cursor[0] -= 1;
-                }
+            if let Event::Resize(_, _) = event {
+                crossterm::terminal::disable_raw_mode().ok();
+                continue;
             }
 
-            else if (event == Event::Key(KeyCode::Right.into())) {
-                if cursor[0] < HEIGHT - 1 {
-                    cursor[0] += 1;
-                }
-            }
+            let code = format!("{:?}", event).split_at(21).1.split(',').next().unwrap().to_string();
 
-            else if (event == Event::Key(KeyCode::Up.into())) {
-                if cursor[1] > 0 {
-                    cursor[1] -= 1;
-                }
-            }
+            match code.as_str() {
+                "Left" => {
+                    if cursor[0] > 0    {
+                        cursor[0] -= 1;
+                    }
+                },
 
-            else if (event == Event::Key(KeyCode::Down.into())) {
-                if cursor[1] < WIDTH - 1 {
-                    cursor[1] += 1;
-                }
-            }
+                "Right" => {
+                    if cursor[0] < HEIGHT - 1 {
+                        cursor[0] += 1;
+                    }
+                },
 
-            else if (event == Event::Key(KeyCode::Char('h').into())) {
-                plate[cursor[1] +  WIDTH * cursor[0]] += 50.
-            }
+                "Up" => {
+                    if cursor[1] > 0 {
+                        cursor[1] -= 1;
+                    }
+                },
 
-            else if (event == Event::Key(KeyCode::Char('c').into()) || event == Event::Key(KeyCode::Char('k').into())) {
-                plate[cursor[1] +  WIDTH * cursor[0]] -= 50.
-            }
-
-            else if (event == Event::Key(KeyCode::Char('+').into())) {
-                lambda += 0.01;
-            }
-
-            else if (event == Event::Key(KeyCode::Char('-').into())) {
-                lambda -= 0.01;
-            }
-
-            else if (event == Event::Key(KeyCode::Char('p').into())) {
-                pause = !pause;
-            }
-
-            else if (event == Event::Key(KeyCode::Char('l').into())) {
-                if (language == "de") {
-                    language = "en";
+                "Down" => {
+                    if cursor[1] < WIDTH - 1 {
+                        cursor[1] += 1;
+                    }
                 }
 
-                else if (language == "en") {
-                    language = "de";
-                }
-            }
+                "Char('h')"                => plate[cursor[1] +  WIDTH * cursor[0]] += 50.,
 
-            else if (event == Event::Key(KeyCode::Esc.into())) {
-                break;
-            }
+                "Char('k')" | "Char('c')"  => plate[cursor[1] +  WIDTH * cursor[0]] -= 50.,
 
-        } else {
-            // No Input was given
+                "Char('+')" => lambda += 0.01,
+
+                "Char('-')" => lambda -= 0.01,
+
+                "Char('p')" => pause = !pause,
+
+                "Char('l')" => {
+                    if (language == "de") {
+                        language = "en";
+                    }
+
+                    else if (language == "en") {
+                        language = "de";
+                    }
+                },
+
+                "Esc" => break,
+
+                _ => {}
+            }
         }
-
         crossterm::terminal::disable_raw_mode().ok();
+        stdout.flush().ok();
     }
-
     crossterm::terminal::disable_raw_mode().ok();
     stdout.execute(terminal::Clear(terminal::ClearType::All)).ok();
 }
